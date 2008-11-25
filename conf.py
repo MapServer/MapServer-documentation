48bb0a1e42e021f5f261e823be951ddcdf7fd36a
Howard Butler <hobu.inc@gmail.com>
2008-11-25 03:36:55 +0000

    make pretty
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@8091 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- conf.py
+++ conf.py
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
 # given in html_static_path.
-html_style = 'default.css'
+html_style = 'sphinxdoc.css'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -123,11 +123,11 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = {"index":"indexsidebar.html"}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
-#html_additional_pages = {}
+#html_additional_pages = {"index":'index.html'}
 
 # If false, no module index is generated.
 #html_use_modindex = True
