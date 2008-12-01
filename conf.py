ec9b4c62f0ba983134d19f6a76c0c852bc16a81e
Howard Butler <hobu.inc@gmail.com>
2008-12-01 17:08:32 +0000

    tweaks for pdf
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@8163 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- conf.py
+++ conf.py
@@ -171,11 +171,11 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = './images/mapserver.jpg'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
-#latex_use_parts = False
+latex_use_parts = True
 
 # Additional stuff for the LaTeX preamble.
 #latex_preamble = ''
