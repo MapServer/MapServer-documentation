b19c56cc5bedcba390572d3744c76f22f5513ff1
Thomas Bonfort <thomas.bonfort@gmail.com>
2008-12-29 21:20:05 +0000

    add slippy map to index page
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@8322 7532c77e-422f-0410-93f4-f0b67bdd69e2

---
+++ _static/ol.js
@@ -0,0 +1,30 @@
+var lon = 261330;
+var lat = 6250266;
+var zoom = 7;
+var map;
+
+function olmapinit(){
+  map = new OpenLayers.Map( 'olmap', {
+restrictedExtent: new OpenLayers.Bounds(243000,6236000,278000,6263000)
+      });
+  
+        var wms = new OpenLayers.Layer.TileCache( "S3", 
+        ['http://mapserverosm.s3.amazonaws.com',
+        'http://s3.amazonaws.com/mapserverosm'],
+        'parisosm',
+        {
+          maxExtent: new OpenLayers.Bounds(-571405,5067685,1061388,6637184),
+          resolutions:[1763.8879363894034,881.9439681947017,352.7775872778807,176.38879363894034,88.19439681947017,35.27775872778806,17.63887936389403,8.819439681947015,3.527775872778806,1.763887936389403],
+          units: 'm',
+          projection:new OpenLayers.Projection("EPSG:900913"),
+          buffer:0,
+          isBaselayer:true
+        } );
+
+map.addLayers([wms]);
+OpenLayers.IMAGE_RELOAD_ATTEMPTS = 3;
+if(!map.getCenter())
+  map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);
+}
+OpenLayers.Event.observe(window, "load", olmapinit);
+
