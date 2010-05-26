dd20babd62e5c116d02c9a09eed3d8a8e331ff65
Howard Butler <hobu.inc@gmail.com>
2010-05-26 16:31:43 +0000

    use our custom sidebar for the main index page (sphinx 1.0+)
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@10178 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ sys.path.append(os.path.abspath('.'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['labels']
+extensions = ['labels' ,'rst2pdf.pdfbuilder']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -32,7 +32,7 @@ templates_path = ['_templates']
 source_suffix = '.txt'
 
 # The encoding of source files.
-#source_encoding = 'utf-8'
+source_encoding = 'utf-8'
 
 # The master toctree document.
 master_doc = 'documentation'
@@ -125,8 +125,9 @@ html_static_path = ['_static']
 html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-html_sidebars = {"index":"indexsidebar.html"}
-
+html_sidebars = {
+    '**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'],
+"index":["indexsidebar.html",'searchbox.html']}
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
 #html_additional_pages = {"index":'index.html'}
