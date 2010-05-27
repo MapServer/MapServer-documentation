277c8510aa17fe9b7857f278903502613a594e58
Alan Boudreault <aboudreault@mapgears.com>
2010-05-27 16:08:40 +0000

    Removed rst2pdf from extensions, not used yet
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@10183 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- conf.py
+++ conf.py
@@ -23,7 +23,8 @@ sys.path.append(os.path.abspath('.'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['labels' ,'rst2pdf.pdfbuilder']
+#extensions = ['labels' ,'rst2pdf.pdfbuilder']
+extensions = ['labels']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
