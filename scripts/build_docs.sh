e410f4bfaafc0a98d0d926dd3295a4eeaab8e9ee
Alan Boudreault <aboudreault@mapgears.com>
2010-09-30 16:13:03 +0000

    Synchronized trunk script/build_docs.sh script in 5.6 branch
    git-svn-id: http://svn.osgeo.org/mapserver/branches/branch-5-6@10555 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -3,13 +3,45 @@ args=("$@")
 LOCATION=${args[0]}
 SVN_BRANCH=${args[1]}
 OUTPUT_LOCATION=${args[2]}
+SVN_TRUNK="svn-trunk"
 
 #LOCATION="/osgeo/mapserver.org"
 LANGUAGES="en de"
 cd $LOCATION/$SVN_BRANCH
+
+# remove all lang dir (except en) to get them updated properly
+for lang in $LANGUAGES;
+do 
+    [ $lang == "en" ] || rm -rf $lang
+done
+
 svn up
 
 cd $LOCATION/$SVN_BRANCH
+
+# Copy all untranslated files in language dir and
+# copy all RFC from trunk 
+for lang in $LANGUAGES;
+do 
+    if [ $lang != "en" ]; then
+	cd en
+	IFS=$'\n'
+	for file in `find . | grep -v .svn`;
+	do
+	    if [ ! -e "../$lang/$file" ]; then
+		cp -r "$file" ../$lang/
+	    fi
+	done
+	unset IFS
+	cd ..
+    fi
+
+    # and copy all RFCs from trunk to branch
+    if [ $SVN_BRANCH != $SVN_TRUNK ]; then
+	[ -e $LOCATION/$SVN_TRUNK/$lang/development/rfc/ ] && cp -f $LOCATION/$SVN_TRUNK/$lang/development/rfc/*.txt $LOCATION/$SVN_BRANCH/$lang/development/rfc/
+    fi
+done
+
 make html
 make latex
 make epub
@@ -20,7 +52,7 @@ do
 	cd $LOCATION/$SVN_BRANCH/build/latex/$lang
 	make all-pdf
 	cp MapServer.pdf $OUTPUT_LOCATION/$lang/
-        cp -r $LOCATION/$SVN_BRANCH/build/html/$lang $OUTPUT_LOCATION/
 done
 
+cp -fr $LOCATION/$SVN_BRANCH/build/html/* $OUTPUT_LOCATION/
 cp $LOCATION/$SVN_BRANCH/build/epub/en/MapServer.epub $OUTPUT_LOCATION/en/MapServer.epub
