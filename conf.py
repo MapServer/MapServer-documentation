818c03f51ab830313301cac6fa0b57b826caea97
Jeff McKenna <jmckenna@gatewaygeomatics.com>
2011-04-17 19:03:16 +0000

    use latest MS logo in MS.pdf
    git-svn-id: http://svn.osgeo.org/mapserver/branches/branch-5-6@11589 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- conf.py
+++ conf.py
@@ -176,7 +176,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = './_static/mapserver.jpg'
+latex_logo = './_static/banner-large.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
