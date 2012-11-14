2d054f71ef77904cf6ecc9c95617446890902ef2
Thomas Bonfort <thomas.bonfort@gmail.com>
2012-11-14 12:32:16 +0100

    make 6.2 the default doc branch

--- scripts/build-all-docs.sh
+++ scripts/build-all-docs.sh
@@ -3,6 +3,6 @@
 export PATH=/usr/local/bin:$PATH
 
 rundate=`date +%Y%m%d-%H%M`
-bash /osgeo/mapserver.org/scripts/build_docs.sh /osgeo/mapserver.org/docs-git branch-6-0 /osgeo/mapserver.org/htdocs/ >> /osgeo/mapserver.org/scripts/cron-logs/log-60-$rundate.txt 2>&1
+bash /osgeo/mapserver.org/scripts/build_docs.sh /osgeo/mapserver.org/docs-git branch-6-2 /osgeo/mapserver.org/htdocs/ >> /osgeo/mapserver.org/scripts/cron-logs/log-62-$rundate.txt 2>&1
 rundate=`date +%Y%m%d-%H%M`
 bash /osgeo/mapserver.org/scripts/build_docs.sh /osgeo/mapserver.org/docs-git master /osgeo/mapserver.org/htdocs/en/trunk >> /osgeo/mapserver.org/scripts/cron-logs/log-trunk-$rundate.txt 2>&1
