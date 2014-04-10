0826421514885fde36d2d86cbc6c19b1cd97c6c2
Thomas Bonfort <thomas.bonfort@gmail.com>
2014-04-10 14:47:43 +0200

    really remove date...

--- conf.py
+++ conf.py
@@ -120,7 +120,7 @@ html_static_path = ['_static']
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 #html_last_updated_fmt = '%A %e %B %Y, %R'
-html_last_updated_fmt = ''
+#html_last_updated_fmt = ''
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
