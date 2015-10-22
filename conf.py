643db2d07e1d0abd846ad2761f8fe13e9e3a7198
Jeff McKenna <jeffmckenna@gmail.com>
2015-10-22 11:58:18 -0300

    trigger a pdf build [build_pdf]

--- conf.py
+++ conf.py
@@ -131,7 +131,7 @@ html_sidebars = {
 #html_use_modindex = True
 
 # If false, no index is generated.
-#html_use_index = True
+html_use_index = True
 
 # If true, the index is split into individual pages for each letter.
 #html_split_index = False
