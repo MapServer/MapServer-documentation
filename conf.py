78094c76a85e1241b34b08bce0c8e84ceb450e62
Jeff McKenna <jeffmckenna@gmail.com>
2016-02-25 17:42:01 -0400

    fix Sphinx warnings

--- conf.py
+++ conf.py
@@ -419,3 +419,15 @@ def setup(app):
     from sphinx.highlighting import lexers
     lexers['wkt'] = WKTLexer()
     lexers['mapfile'] = MapFileLexer()
+    
+# patch Sphinx to avoid warnings of "nonlocal image URI found"
+import sphinx.environment
+from docutils.utils import get_source_line
+
+def _warn_node(self, msg, node):
+    if not msg.startswith('nonlocal image URI found:'):
+        self._warnfunc(msg, '%s:%s' % get_source_line(node))
+
+sphinx.environment.BuildEnvironment.warn_node = _warn_node
+
+
