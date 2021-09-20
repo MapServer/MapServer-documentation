1590be91b524d80b15152d70d22f60f0bd8e3fa9
sethg <sethg@geographika.co.uk>
2021-09-20 17:36:12 +0200

    Build master docs on new gh-pages branch

---
+++ scripts/travis_update_pages.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+builddir=$1
+destdir=$2
+
+git config user.email "mapserverbot@mapserver.bot"
+git config user.name "MapServer deploybot"
+
+# clone without any existing files
+git clone --no-checkout --depth=1 https://geographika:$GITHUB_TOKEN@github.com/geographika/MapServer-documentation $destdir/MapServer-documentation
+cd $destdir/MapServer-documentation
+git checkout -B gh-pages
+
+# delete existing files
+git rm -r * --quiet
+
+# add in the new build files
+cp -rf "$builddir/html/"* $destdir/MapServer-documentation
+
+cd $destdir/MapServer-documentation
+touch .nojekyll
+
+git add -A
+git commit -m "update with results of commit https://github.com/mapserver/MapServer-documentation/commit/$TRAVIS_COMMIT" --quiet
+git remote set-url origin https://geographika:$GITHUB_TOKEN@github.com/geographika/MapServer-documentation
+git push origin gh-pages --force
