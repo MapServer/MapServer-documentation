51fe798ff7e00bb9f9158bbd0103669ca546ceda
sethg <sethg@geographika.co.uk>
2021-09-21 10:11:18 +0200

    Switch to SSH

--- scripts/travis_update_pages.sh
+++ scripts/travis_update_pages.sh
@@ -7,7 +7,8 @@ git config user.email "mapserverbot@mapserver.bot"
 git config user.name "MapServer deploybot"
 
 # clone without any existing files
-git clone --no-checkout --depth=1 https://geographika:$GITHUB_TOKEN@github.com/geographika/MapServer-documentation $destdir/MapServer-documentation
+git clone --no-checkout --depth=1 git@github.com:mapserver/mapserver.github.io.git $destdir/MapServer-documentation
+
 cd $destdir/MapServer-documentation
 git checkout -B gh-pages
 
@@ -22,5 +23,4 @@ touch .nojekyll
 
 git add -A
 git commit -m "update with results of commit https://github.com/mapserver/MapServer-documentation/commit/$TRAVIS_COMMIT" --quiet
-#git remote set-url origin https://geographika:$GITHUB_TOKEN@github.com/geographika/MapServer-documentation
 git push origin gh-pages --force
