6749437a91504de1e13b1d8a0f1bfc6686a72bea
sethg <sethg@geographika.co.uk>
2021-09-21 10:07:39 +0200

    Test

--- scripts/travis_update_pages.sh
+++ scripts/travis_update_pages.sh
@@ -7,7 +7,7 @@ git config user.email "mapserverbot@mapserver.bot"
 git config user.name "MapServer deploybot"
 
 # clone without any existing files
-git clone --no-checkout --depth=1 https://mapserver:$GITHUB_TOKEN@github.com/mapserver/MapServer-documentation $destdir/MapServer-documentation
+git clone --no-checkout --depth=1 https://geographika:$GITHUB_TOKEN@github.com/geographika/MapServer-documentation $destdir/MapServer-documentation
 cd $destdir/MapServer-documentation
 git checkout -B gh-pages
 
@@ -22,5 +22,5 @@ touch .nojekyll
 
 git add -A
 git commit -m "update with results of commit https://github.com/mapserver/MapServer-documentation/commit/$TRAVIS_COMMIT" --quiet
-git remote set-url origin https://mapserver:$GITHUB_TOKEN@github.com/mapserver/MapServer-documentation
+#git remote set-url origin https://geographika:$GITHUB_TOKEN@github.com/geographika/MapServer-documentation
 git push origin gh-pages --force
