62449fa256707be4d25da488eee202f8d683d34f
Jeff McKenna <jmckenna@gatewaygeomatics.com>
2025-04-14 09:05:40 -0300

    update links

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -13,4 +13,4 @@ uname=`uname`
 if [ "$uname" = "Darwin" ]; then
   SED=gsed
 fi
-git --no-pager  log --no-merges  --pretty=format:'* %s (%an) : `%h <https://github.com/mapserver/mapserver/commit/%H>`__' $tags  | $SED  's!#\([0-9]\+\)! `#\1 <https://github.com/mapserver/mapserver/issues/\1>`__ !g'
+git --no-pager  log --no-merges  --pretty=format:'* %s (%an) : `%h <https://github.com/MapServer/MapServer/commit/%H>`__' $tags  | $SED  's!#\([0-9]\+\)! `#\1 <https://github.com/MapServer/MapServer/issues/\1>`__ !g'
